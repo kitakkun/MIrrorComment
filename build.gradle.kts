51a065caa7a7786e2b3e88a8b488fc71bbc43d84
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-07-23 22:21:49 +0900

    chore: migrate to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,31 +24,24 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 implementation(compose.desktop.currentOs)
-                implementation("org.seleniumhq.selenium:selenium-java:4.10.0")
-                implementation("io.insert-koin:koin-core:3.1.2")
+                implementation(libs.selenium)
+                implementation(libs.koin.core)
 
                 // kt-vox
-                implementation("com.github.kitakkun:kt-vox:0.0.3")
-                val retrofitVersion = "2.9.0"
-                implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+                implementation(libs.ktvox)
+                implementation(libs.retrofit)
 
-                val voyagerVersion = "1.0.0-rc06"
-                // Navigator
-                implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion")
-                // BottomSheetNavigator
-                implementation("cafe.adriel.voyager:voyager-bottom-sheet-navigator:$voyagerVersion")
-                // TabNavigator
-                implementation("cafe.adriel.voyager:voyager-tab-navigator:$voyagerVersion")
-                // Transitions
-                implementation("cafe.adriel.voyager:voyager-transitions:$voyagerVersion")
+                // voyager
+                implementation(libs.voyager.navigator)
+                implementation(libs.voyager.bottom.sheet.navigator)
+                implementation(libs.voyager.tab.navigator)
+                implementation(libs.voyager.transitions)
 
-                implementation("org.jetbrains.compose.material:material-icons-extended:1.4.1")
-                implementation("io.github.bonigarcia:webdrivermanager:5.0.3")
+                implementation(libs.material.icons.extended)
+                implementation(libs.webdrivermanager)
 
-                implementation("io.coil-kt:coil-compose:2.4.0")
-
-                implementation("com.godaddy.android.colorpicker:compose-color-picker:0.7.0")
-                implementation("com.godaddy.android.colorpicker:compose-color-picker-jvm:0.7.0")
+                implementation(libs.compose.color.picker)
+                implementation(libs.compose.color.picker.jvm)
             }
         }
         val jvmTest by getting
